7635f670323daa43757c6976be60d2301a2bcf10
bharani <bharanikumar070@gmail.com>
2023-07-08 09:24:12 +0530

    postgres configuration

---
+++ management/commands/createsu.py
@@ -0,0 +1,23 @@
+import os
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser."
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="bharani09").exists():
+            password = os.environ.get("SUPERUSER_PASSWORD")
+            if password is None:
+                raise ValueError("Password not found")
+            User.objects.create_superuser(
+                first_name='bharani'
+                last_name='kumar'
+                username="bharani09",
+                email="bharanikumar070@gmail.com", 
+                password=bharani070,
+            )
+            print("Superuser has been created.")
+        else:
+            print("Superuser exists")
